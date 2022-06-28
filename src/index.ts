2379fb55fcd4ea4cb5b54bda13b909b6486b96cb
Yun Shan <yunshan21st@qq.com>
2022-06-29 01:16:21 +0800

    调整一下构建输出，增加readme

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-import { DynamicItem, DynamicType } from './types/dynamic/Dynamic';
-import { RichTextNode, RichTextNodeType } from './types/dynamic/RichTextNode';
-
-const a: RichTextNode = {} as any;
-if (a.type === RichTextNodeType.TOPIC) {
-  a.text = '111';
-}
-
-export {DynamicType, DynamicItem, RichTextNode, RichTextNodeType};
+export * from './types/dynamic';
---
+++ src/types/dynamic/index.ts
@@ -0,0 +1,4 @@
+export * from './Dynamic';
+export * from './DynamicModule';
+export * from './ModuleDynamicMajor';
+export * from './RichTextNode';
