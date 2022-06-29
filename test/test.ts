0f6b0808e28998800a0c4886be05fc66ca659511
Yun Shan <yunshan21st@qq.com>
2022-06-29 13:37:13 +0800

    测试使用程序化构建json schema、工作流仅在修改源码时运行

--- test/test.ts
+++ test/test.ts
@@ -3,8 +3,21 @@ const validate = require('jsonschema').validate;
 const fs = require('fs');
 const path = require('path');
 
+const {createGenerator, Config} = require('ts-json-schema-generator');
+
+const config: typeof Config = {
+  path: './src/types/dynamic/Dynamic.ts',
+  tsconfig: './tsconfig.json',
+  type: 'DynamicItem',
+  expose: 'all',
+  strictTuples: true,
+  encodeRefs: false,
+};
+
+const schema = createGenerator(config).createSchema(config.type);
+
 function readFile(filename: string) {
-  return JSON.parse(fs.readFileSync(path.resolve(filename)));
+  return JSON.parse(fs.readFileSync(path.resolve(filename)).toString());
 }
 
 interface DynamicList {
@@ -19,7 +32,7 @@ describe('B站动态解析', function() {
   it('验证动态是否完全可解析', function() {
     const list = readFile('test/data/dynamic-list.json') as DynamicList;
     for (const item of list.items) {
-      const result = validate(item, readFile('gen/schema.json'), {allowUnknownAttributes: false, nestedErrors: true});
+      const result = validate(item, schema, {allowUnknownAttributes: false, nestedErrors: true});
       assert.equal(result.errors.length, 0);
     }
   });
